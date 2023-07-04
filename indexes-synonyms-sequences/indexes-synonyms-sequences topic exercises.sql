ab05709f9aacbc2c690e55b88f3f35950ac71e68
sameerkewal <sameerkewal1@gmail.com>
2023-07-04 12:57:04 -0300

    finished topic exercises

--- indexes-synonyms-sequences/indexes-synonyms-sequences topic exercises.sql
+++ indexes-synonyms-sequences/indexes-synonyms-sequences topic exercises.sql
@@ -67,30 +67,158 @@ create synonym cntries for hr.countries;
 
 /* Managing SEQUENCE */
 --1 Create a sequence called "EMPLOYEE_ID_SEQ" that starts with 1000 and increments by 1.
-create or replace sequence employee_id_seq
+create sequence employee_id_seq
 start with 1000
-increment by -1;
+increment by 1;
 
 
---2 Insert a new employee into the "EMPLOYEES" table using the next value from the "EMPLOYEE_ID_SEQ" sequence for the "EMPLOYEE_ID" column.
+--2 Insert a new employee into the "EMPLOYEES" table using the next value from the "EMPLOYEE_ID_SEQ" sequence 
+---for the "EMPLOYEE_ID" column.
+create table emp_test(
+    id number default employee_id_seq.nextval not null,
+    name varchar2(20)
+);
+
+
+insert into emp_test values(1, 'sameer');
+insert into emp_test values(EMPLOYEE_ID_SEQ.NEXTVAL, 'sameer');
+insert into emp_test values(1002, 'sameer');
+insert into emp_test values(EMPLOYEE_ID_SEQ.NEXTVAL, 'sameer');
+
+select EMPLOYEE_ID_SEQ.CURRVAL from dual;
+
+select * from emp_test;
+
 --3 Retrieve the current value of the "EMPLOYEE_ID_SEQ" sequence.
---4 Increment the "EMPLOYEE_ID_SEQ" sequence by 10 manually.
---5 Alter the "EMPLOYEE_ID_SEQ" sequence to restart from 2000 and increment by 2.
---6 Remove the "EMPLOYEE_ID_SEQ" sequence from the database.
---7 Create a sequence called "DEPARTMENT_ID_SEQ" that starts with 10 and increments by 10.
---8 Use the "DEPARTMENT_ID_SEQ" sequence to insert a new department into the "DEPARTMENTS" table, providing values for other columns as well.
---9 Retrieve the next value from the "DEPARTMENT_ID_SEQ" sequence.
---10 Insert multiple departments into the "DEPARTMENTS" table using the "DEPARTMENT_ID_SEQ" sequence.
---11 Alter the "DEPARTMENT_ID_SEQ" sequence to restart from 100 and increment by 1.
---12 Drop the "EMPLOYEE_ID_SEQ" and "DEPARTMENT_ID_SEQ" sequences.
+select EMPLOYEE_ID_SEQ.CURRVAL from dual;
 
+
+--4 Increment the "EMPLOYEE_ID_SEQ" sequence by 10 manually.
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+select employee_id_seq.nextval from dual;
+
+
+select EMPLOYEE_ID_SEQ.CURRVAL from dual;
+
+
+
+--5 Alter the "EMPLOYEE_ID_SEQ" sequence to restart from 2000 and increment by 2.
+
+alter sequence EMPLOYEE_ID_SEQ 
+INCREMENT by 1000;
+
+select EMPLOYEE_ID_SEQ.NEXTVAL from dual;
+
+alter sequence EMPLOYEE_ID_SEQ MINVALUE 2000
+increment by 2;
+
+
+--6 Remove the "EMPLOYEE_ID_SEQ" sequence from the database.
+drop sequence EMPLOYEE_ID_SEQ;
+
+--7 Create a sequence called "DEPARTMENT_ID_SEQ" that starts with 10 and increments by 10.
+create sequence DEPARTMENT_ID_SEQ 
+start with 10
+increment by 10;
+
+
+--8 Use the "DEPARTMENT_ID_SEQ" sequence to insert a new department into the "DEPARTMENTS" table, 
+--providing values for other columns as well.
+
+--Bruh ik heb me antwoord perongeluk weggehaald
+
+
+--9 Retrieve the next value from the "DEPARTMENT_ID_SEQ" sequence.
+
+select DEPARTMENTS_ID_SEQ.CURRVAL from dual;
+select DEPARTMENT_ID_SEQ.CURRVAL from dual;
+
+--10 Insert multiple departments into the "DEPARTMENTS" table using the "DEPARTMENT_ID_SEQ" sequence.
+
+drop table DEPT_COPY;
+
+create table DEPT_COPY(
+    DEPARTMENT_ID number constraint dept_copy_pk primary key,
+    DEPARTMENT_NAME varchar2(20),
+    MANAGER_ID constraint dept_copy_mang_fk references EMPLOYEES(EMPLOYEE_ID),
+    LOCATION_ID constraint dept_copy_loc_fk references LOCATIONS(LOCATION_ID)
+);
+
+insert into DEPT_COPY values(DEPARTMENT_ID_SEQ.NEXTVAL, 'HR', null, null);
+insert into DEPT_COPY values(DEPARTMENT_ID_SEQ.NEXTVAL, 'Finance', null, null);
+insert into DEPT_COPY values(DEPARTMENT_ID_SEQ.NEXTVAL, 'Marketing', null, null);
+insert into DEPT_COPY values(DEPARTMENT_ID_SEQ.NEXTVAL, 'IT', null, null);
+
+
+select DEPARTMENT_ID_SEQ.CURRVAL from dual;
+
+
+--11 Alter the "DEPARTMENT_ID_SEQ" sequence to restart from 100 and increment by 1.
+alter sequence DEPARTMENTS_SEQ
+MINVALUE 100
+increment by 1;
+
+--12 Drop the "EMPLOYEE_ID_SEQ" and "DEPARTMENT_ID_SEQ" sequences.
+drop sequence department_ID_SEQ;
 
 /* Managing indexes */
 --1 Create an index named "IDX_EMPLOYEE_LAST_NAME" on the "LAST_NAME" column of the "EMPLOYEES" table.
+create index IDX_EMPLOYEE_LAST_NAME on EMPLOYEES(LAST_NAME);
+
+
 --2 Write a query to retrieve all employees with the last name 'Smith' using the created index.
+select * from EMPLOYEES where LAST_NAME ='Smith';
+
 --3 Add a new column, such as "EMAIL", to the index "IDX_EMPLOYEE_LAST_NAME" without recreating the entire index.
+alter index IDX_EMPLOYEE_LAST_NAME on EMPLOYEES(LAST_NAME, EMAIL);
+
+
+
 --4 Remove the "IDX_EMPLOYEE_LAST_NAME" index from the database.
+drop index IDX_EMPLOYEE_LAST_NAME;
+
+
 --5 Attempt to insert a new job into the "JOBS" table with the same job title as an existing row to observe the unique index constraint.
+
+
+create unique index job_title_idx on jobs(JOB_TITLE);
+
+insert into jobs values('PRES', 'President', 2000, 9000);
+
+
+
+
+SELECT * FROM JOBS;
+
+select * from USER_INDEXES where table_name='JOBS';
+
+select * from USER_CONSTRAINTS where table_name='JOBS';
+
+
 --6 Temporarily disable the "IDX_JOB_TITLE" unique index to allow inserting duplicate job titles.
+alter index job_title_idx UNUSABLE;
+
+
+alter table jobs disable constraint job_title_idx;
+
+alter index JOB_TITLE_idx disable;
+
+
+insert into jobs values('PRES', 'President', 2000, 2000);
+
+select * from USER_INDEXES where table_name='JOBS';
+select * from USER_IND_COLUMNS where table_name='JOBS';
+
+
 --7 Enable the "IDX_JOB_TITLE" unique index to enforce the uniqueness constraint again.
 --8 Remove the "IDX_JOB_TITLE" unique index from the database.
+
+drop index job_title_idx;
